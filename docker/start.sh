9e50888ca210742e407602a14a3b03419d7d5e0b
KhurtSC <khurtcalicdan@gmail.com>
2026-06-04 11:17:57 +0800

    w

--- docker/start.sh
+++ docker/start.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 set -e
-
-# Start php-fpm first
 php-fpm -D
-
-# Wait for php-fpm to be ready
 sleep 2
-
-# Run Laravel commands
 php artisan migrate --force
+php artisan db:seed --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-# Start nginx in foreground (keeps container alive)
 nginx -g 'daemon off;'
