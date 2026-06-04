9db628ed91304f66afc4808075e69ed93fcec94c
KhurtSC <khurtcalicdan@gmail.com>
2026-06-04 11:48:37 +0800

    m

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
     $middleware->trustProxies(at: '*');
+    $middleware->alias([
+        'admin'   => \App\Http\Middleware\AdminMiddleware::class,
+        'cashier' => \App\Http\Middleware\CashierMiddleware::class,
+    ]);
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
