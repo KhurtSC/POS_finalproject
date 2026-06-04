bf91d5bd3d5b1cbcaead45543c8feaa902533b4b
KhurtSC <khurtcalicdan@gmail.com>
2026-06-04 10:49:02 +0800

    deploy

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+set -e
+
+# Start php-fpm first
+php-fpm -D
+
+# Wait for php-fpm to be ready
+sleep 2
+
+# Run Laravel commands
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php-fpm -D && nginx -g 'daemon off;'
+
+# Start nginx in foreground (keeps container alive)
+nginx -g 'daemon off;'
